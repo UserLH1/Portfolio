66604a8ec5f3ce4efadaa40ff84b0ab7da3763b5
UserLH1 <lazeahoratiu@gmail.com>
2024-09-05 15:43:55 +0300

    added projects section and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Encryption from "@/components/main/Encryption";
+import Footer from "@/components/main/Footer";
 import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
-
 export default function Home() {
   return (
     <main className="w-full h-full bg-[#030014]">
@@ -9,6 +10,8 @@ export default function Home() {
         <Hero />
         <Skills />
         <Encryption />
+        <Projects />
+        <Footer />
       </div>
     </main>
   );
